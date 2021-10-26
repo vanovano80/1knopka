d34754e2d73ddd87ff159aa7329174ae21716595
vanovano80 <ivan.utkin.2006@mail.ru>
2021-10-26 20:32:48 +0300

    cool

---
+++ build/precache-manifest.b9b6b9880226e81ebf2c6bcb4f5fdfbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0823694753178a5b0f51a065a7faf27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44693d6cb3b587819189",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "58960bea9bf8cd0087e5",
+    "url": "/static/css/main.b1dfec6e.chunk.css"
+  },
+  {
+    "revision": "44693d6cb3b587819189",
+    "url": "/static/js/2.edba615f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.edba615f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58960bea9bf8cd0087e5",
+    "url": "/static/js/main.8367f36b.chunk.js"
+  },
+  {
+    "revision": "8f3f64d0671efe8159c3",
+    "url": "/static/js/runtime-main.3807a136.js"
+  }
+]);
